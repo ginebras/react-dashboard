025156c4e4e92a1fb1fbecaada76e3247411fabe
ginebras <alenicofranco@gmail.com>
2022-04-11 00:17:44 -0300

    3 files modified

--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -4,7 +4,94 @@ import './WidgetSm.css';
 export default function WidgetSm() {
   return (
     <div className="widgetSm">
-      <p className="class">sm</p>
+      <h2 className="widgetSmTitle">New Join Members</h2>
+      <ul className="usersList">
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Carol Montenegro</span>
+            <span className="widgetUserTitle">Software Engineer</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Carol Montenegro</span>
+            <span className="widgetUserTitle">Software Engineer</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Carol Montenegro</span>
+            <span className="widgetUserTitle">Software Engineer</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Carol Montenegro</span>
+            <span className="widgetUserTitle">Software Engineer</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Carol Montenegro</span>
+            <span className="widgetUserTitle">Software Engineer</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
