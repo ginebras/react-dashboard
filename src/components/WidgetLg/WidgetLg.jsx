5b1de9399e51b96c073f79bf8e7e337cf928af73
ginebras <alenicofranco@gmail.com>
2022-04-11 14:49:05 -0300

    4 files modified

--- src/components/WidgetLg/WidgetLg.jsx
+++ src/components/WidgetLg/WidgetLg.jsx
@@ -3,6 +3,8 @@ import './WidgetLg.css';
 
 export default function WidgetLg() {
   const type = 'Aproved';
+  const type1 = 'Decline';
+  const type2 = 'Pending';
   return (
     <div className="widgetLg">
       <h2 className="widgetLgTitle">Latest Transactions</h2>
@@ -29,7 +31,22 @@ export default function WidgetLg() {
             <td className="widgetLgDate">2 Jun 2021</td>
             <td className="widgetLgAmount">$122.00</td>
             <td className="widgetLgTd">
-              <button className={`widgetLgButton ${type}`}>Approved</button>
+              <button className={`widgetLgButton ${type}`}>{type}</button>
+            </td>
+          </tr>
+          <tr>
+            <td className="widgetLgUser">
+              <img
+                src="https://images.unsplash.com/photo-1649228537048-227a135e86d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"
+                alt="customer-avatar"
+                className="widgetLgImg"
+              />
+              <span>Camila Susan</span>
+            </td>
+            <td className="widgetLgDate">2 Jun 2021</td>
+            <td className="widgetLgAmount">$122.00</td>
+            <td className="widgetLgTd">
+              <button className={`widgetLgButton ${type1}`}>{type1}</button>
             </td>
           </tr>
           <tr>
@@ -59,22 +76,7 @@ export default function WidgetLg() {
             <td className="widgetLgDate">2 Jun 2021</td>
             <td className="widgetLgAmount">$122.00</td>
             <td className="widgetLgTd">
-              <button className={`widgetLgButton ${type}`}>Approved</button>
-            </td>
-          </tr>
-          <tr>
-            <td className="widgetLgUser">
-              <img
-                src="https://images.unsplash.com/photo-1649228537048-227a135e86d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"
-                alt="customer-avatar"
-                className="widgetLgImg"
-              />
-              <span>Camila Susan</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgTd">
-              <button className={`widgetLgButton ${type}`}>Approved</button>
+              <button className={`widgetLgButton ${type2}`}>{type2}</button>
             </td>
           </tr>
         </tbody>
