b51c1a0a95662780f02164d3d6cde34c96fd1be4
ginebras <alenicofranco@gmail.com>
2022-04-12 01:58:43 -0300

    2 files modified

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+
 import './Products.css';
 
+import { productRows } from '../../dummyData';
+
 export default function Products() {
+  const [data, setData] = useState(productRows);
+
+  const handleDelete = (e) => {
+    setData(data.filter((user) => user.id !== e));
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'product',
+      headerName: 'Product',
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="productListUser">
+            <img
+              className="productListImg"
+              src={params.row.img}
+              alt="avatar-row"
+            />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: 'stock', headerName: 'Stock', width: 200 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 120,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      width: 160,
+    },
+    {
+      field: 'action',
+      headerName: 'Actions',
+      width: 120,
+      renderCell: (params) => {
+        return (
+          <div className="actionsContainer">
+            <Link to={`/product-details/${params.row.id}`}>
+              <button className="editBtn">Edit</button>
+            </Link>
+            <i
+              class="bi bi-trash-fill table-icon"
+              onClick={(e) => handleDelete(params.row.id)}
+            ></i>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="products">
-      <h1>Products</h1>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
     </div>
   );
 }
