07a16fecd926ce5ab80187d9bd16963ed7453d19
ginebras <alenicofranco@gmail.com>
2022-04-12 01:10:09 -0300

    2 files modified

--- src/pages/NewUser/NewUser.jsx
+++ src/pages/NewUser/NewUser.jsx
@@ -4,7 +4,106 @@ import './NewUser.css';
 export default function NewUser() {
   return (
     <div className="newUser">
-      <h1>newuser</h1>
+      <h1 className="newUserTitle">New User</h1>
+
+      <form>
+        <div className="formGroup">
+          <label htmlFor="username" className="labelInput">
+            Username
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            id="username"
+            placeholder="john"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="fullname" className="labelInput">
+            Full Name
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            placeholder="John Smith"
+            id="fullname"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="email" className="labelInput">
+            Email
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            id="email"
+            placeholder="john@gmail.com"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="password" className="labelInput">
+            Password
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            placeholder="password"
+            id="password"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="phone" className="labelInput">
+            Phone
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            placeholder="+1 123 456 78"
+            id="phone"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="address" className="labelInput">
+            Address
+          </label>
+          <input
+            type="text"
+            className="formInput"
+            id="address"
+            placeholder="New York,USA"
+          />
+        </div>
+
+        <div className="formGroup">
+          <label className="labelInput">Gender</label>
+          <div className="newGender">
+            <input className="inputRadio" type="radio" id="male" value="male" />
+            <label htmlFor="male">Male</label>
+
+            <input className="inputRadio" type="radio" id="female" value="female" />
+            <label htmlFor="female">Female</label>
+
+            <input className="inputRadio" type="radio" id="other" value="other" />
+            <label htmlFor="other">Other</label>
+          </div>
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="active" className="labelInput">
+            Active
+          </label>
+          <select className="formInput">
+            <option disabled>Select option</option>
+            <option>Yes</option>
+            <option>No</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 }
